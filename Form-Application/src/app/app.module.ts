288d3487847ee59d4d8a36fb3b9496fc0dcf8598
Piumi Perera <piumidp96@gmail.com>
2022-04-08 15:21:23 +0530

    Create product component and service

--- Form-Application/src/app/app.module.ts
+++ Form-Application/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './components/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
